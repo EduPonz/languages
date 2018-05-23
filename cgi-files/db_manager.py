8428e761a6540c1d1192973597c05a7d24bc70cd
EduPonz <e.ponzs@gmail.com>
2018-05-23 10:07:49 +0200

    login and update user features. User identifies by user id. Create tables for users

--- cgi-files/db_manager.py
+++ cgi-files/db_manager.py
@@ -5,6 +5,7 @@
 import cgi
 import sys
 import sqlite3
+import uuid
 
 TABLE_VERBS = 'verbs'
 COLUMN_ID = 'id'
@@ -27,6 +28,7 @@ COLUMN_PLURAL_DEF_DAN = 'plural_define'
 COLUMN_PLURAL_DEF_ENG = 'plural_define_english'
 
 TABLE_USERS = 'users'
+COLUMN_USER_ID = 'user_id'
 COLUMN_USERNAME = 'username'
 COLUMN_PASSWORD = 'password'
 COLUMN_EMAIL = 'e_mail'
@@ -38,10 +40,10 @@ def output(content):
     sys.stdout.write(content)
 
 
-def create_verbs_table():
+def create_verbs_table(table_name):
     """."""
     try:
-        db.execute('CREATE TABLE IF NOT EXISTS ' + TABLE_VERBS +
+        db.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
                    ' (' + COLUMN_ID +
                    ' INTEGER PRIMARY KEY AUTOINCREMENT, ' +
                    COLUMN_INFITIVE + ' TEXT NOT NULL, ' +
@@ -52,16 +54,16 @@ def create_verbs_table():
                    COLUMN_TIMES_ASKED + ' TEXT, ' +
                    COLUMN_TIMES_RIGHT + ' TEXT)')
 
-        output('Table "' + TABLE_VERBS + '" created!')
+        output('Table "' + table_name + '" created!')
     except Exception as e:
-        output('Table "' + TABLE_VERBS + '" NOT created. ' +
+        output('Table "' + table_name + '" NOT created. ' +
                'Exception {}'.format(e))
 
 
-def create_nouns_table():
+def create_nouns_table(table_name):
     """."""
     try:
-        db.execute('CREATE TABLE IF NOT EXISTS ' + TABLE_NOUNS +
+        db.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
                    ' (' + COLUMN_ID +
                    ' INTEGER PRIMARY KEY AUTOINCREMENT, ' +
                    COLUMN_DEF_DAN + ' TEXT NOT NULL, ' +
@@ -75,38 +77,42 @@ def create_nouns_table():
                    COLUMN_TIMES_ASKED + ' TEXT, ' +
                    COLUMN_TIMES_RIGHT + ' TEXT)')
 
-        output('Table "' + TABLE_NOUNS + '" created!')
+        output('Table "' + table_name + '" created!')
     except Exception as e:
-        output('Table "' + TABLE_NOUNS + '" NOT created. ' +
+        output('Table "' + table_name + '" NOT created. ' +
                'Exception {}'.format(e))
 
 
-def create_users_table():
+def create_users_table(table_name):
     """."""
     try:
-        db.execute('CREATE TABLE IF NOT EXISTS ' + TABLE_USERS +
+        db.execute('CREATE TABLE IF NOT EXISTS ' + table_name +
                    ' (' + COLUMN_ID +
                    ' INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+                   COLUMN_USER_ID + ' TEXT NOT NULL, ' +
                    COLUMN_USERNAME + ' TEXT NOT NULL, ' +
                    COLUMN_PASSWORD + ' TEXT NOT NULL, ' +
                    COLUMN_EMAIL + ' TEXT NOT NULL)')
 
-        output('Table "' + TABLE_USERS + '" created!')
+        output('Table "' + table_name + '" created!')
     except Exception as e:
-        output('Table "' + TABLE_USERS + '" NOT created. ' +
+        output('Table "' + table_name + '" NOT created. ' +
                'Exception {}'.format(e))
 
 
 def insert_user(username, password, e_mail):
     """."""
-    params = (username, password, e_mail)
+    user_id = str(uuid.uuid4())
+    user_id = user_id.replace("-", "")
+    params = (user_id, username, password, e_mail)
     try:
         if check_username_availability(username) is True:
             db.execute('INSERT INTO ' + TABLE_USERS + ' (' +
+                       COLUMN_USER_ID + ', ' +
                        COLUMN_USERNAME + ', ' +
                        COLUMN_PASSWORD + ', ' +
                        COLUMN_EMAIL + ') ' +
-                       ' VALUES (?,?,?)',
+                       ' VALUES (?,?,?,?)',
                        params)
 
             db.commit()
@@ -127,28 +133,77 @@ def check_username_availability(username):
             return False
         else:
             return True
-    except:
-        output('Cannot check username availability')
+    except Exception as e:
+        output('Cannot check username availability. Exception {}'.format(e))
         return None
 
+
+def check_login(username, password):
+    """."""
+    try:
+        cursor.execute('SELECT ' +
+                       COLUMN_USERNAME + ', ' +
+                       COLUMN_PASSWORD +
+                       ' FROM ' + TABLE_USERS +
+                       ' WHERE ' + COLUMN_USERNAME + ' = "' + username +
+                       '" AND ' + COLUMN_PASSWORD + ' = "' + password + '"')
+        if len(cursor.fetchall()) > 0:
+            output('login correct')
+            return True
+        else:
+            output('login not correct')
+            return False
+    except Exception as e:
+        output('Cannot check login. Exception {}'.format(e))
+        return None
+
+
+def get_user_id(username):
+    """."""
+    try:
+        cursor.execute('SELECT ' + COLUMN_USER_ID +
+                       ' FROM ' + TABLE_USERS +
+                       ' WHERE ' + COLUMN_USERNAME + ' = "' + username + '"')
+
+        user_id = cursor.fetchone()[0]
+        return user_id
+
+    except Exception as e:
+        output('Cannot get user id. Exception {}'.format(e))
+
+
+def update_user(user_id, new_username, new_password, new_email):
+    """."""
+    params = (new_username, new_password, new_email, user_id)
+    try:
+        cursor.execute('UPDATE ' + TABLE_USERS +
+                       ' SET ' +
+                       COLUMN_USERNAME + ' = ?, ' +
+                       COLUMN_PASSWORD + ' = ?, ' +
+                       COLUMN_EMAIL + ' = ?' +
+                       ' WHERE ' + COLUMN_USER_ID + ' = ?',
+                       params)
+        db.commit()
+        output('User updated!')
+    except Exception as e:
+        output('Cannot update user. Exception {}'.format(e))
+
+
 form = cgi.FieldStorage()
-# output('form {}'.format(form))
 db_path = '/var/www/html/languages/database/danish.db'
 db = sqlite3.connect(db_path)
 cursor = db.cursor()
-
-# insert_user('edu', 'e11235813', 'e.ponzs@gmail.com')
 
 try:
     if 'create table' in form:
         table_name = str(form['create table'].value)
 
         if table_name == 'verbs':
-            create_verbs_table()
+            create_verbs_table(table_name)
         elif table_name == 'nouns':
-            create_nouns_table()
+            create_nouns_table(table_name)
         elif table_name == 'users':
-            create_users_table()
+            create_users_table(table_name)
 
     elif 'insert' in form:
         table_name = str(form['insert'].value)
@@ -158,6 +213,32 @@ try:
             password = str(form['password'].value)
             e_mail = str(form['email'].value)
             insert_user(username, password, e_mail)
+
+    elif 'login' in form:
+
+        username = str(form['username'].value)
+        password = str(form['password'].value)
+
+        if check_login(username, password):
+            user_id = get_user_id(username)
+            append_name = '_' + user_id
+            table_verbs = TABLE_VERBS + append_name
+            table_nouns = TABLE_NOUNS + append_name
+
+            create_verbs_table(table_verbs)
+            create_nouns_table(table_nouns)
+
+    elif 'update_user' in form:
+        username = str(form['username'].value)
+        password = str(form['password'].value)
+
+        if check_login(username, password):
+            user_id = get_user_id(username)
+            new_username = str(form['new_username'].value)
+            new_password = str(form['new_password'].value)
+            new_email = str(form['new_email'].value)
+            update_user(user_id, new_username, new_password, new_email)
+
 except:
     pass
 
--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,8 @@
 function onCreate() {
 	document.getElementById("title").innerHTML = "Development page";
+	document.getElementById("signup_title").innerHTML = "Sign up";
+	document.getElementById("loging_title").innerHTML = "Log in";
+	document.getElementById("update_user_title").innerHTML = "Update user";
 	create_tables();
 }
 
@@ -55,6 +58,14 @@ function requestCallback(response) {
 		document.getElementById("password").value = '';
 		document.getElementById("confirm_password").value = '';
 		document.getElementById("e_mail").value = '';
+	
+	} else if (response == 'login correct' || response == 'Cannot check login') {
+		document.getElementById("result_login").innerHTML = response;
+		document.getElementById("username_login").value = '';
+		document.getElementById("password_login").value = '';
+
+	} else if (response == 'login not correct') {
+		document.getElementById("result_login").innerHTML = response;
 	}
 }
 
@@ -92,6 +103,41 @@ function insert_user() {
 		url += '&username=' + encodeURIComponent(username);
 		url += '&password=' + encodeURIComponent(password);
 		url += '&email=' + encodeURIComponent(e_mail);
+		callAjax(url, requestCallback);
+	}
+}
+
+function check_login() {
+	var username = document.getElementById("username_login").value;
+	var password = document.getElementById("password_login").value;
+
+	var url;
+	url = '/cgi-bin/db_manager.py?';
+	url += 'login=' + encodeURIComponent('login');
+	url += '&username=' + encodeURIComponent(username);
+	url += '&password=' + encodeURIComponent(password);
+	callAjax(url, requestCallback);
+}
+
+function update_user() {
+	var username = document.getElementById("update_username").value;
+	var password = document.getElementById("update_password").value;
+	var new_username = document.getElementById("update_new_username").value;
+	var new_password = document.getElementById("update_new_password").value;
+	var confirm_password = document.getElementById("update_new_confirm_pwd").value;
+	var new_email = document.getElementById("update_new_e_mail").value;
+
+	if (new_password != confirm_password) {
+		document.getElementById("update_result").innerHTML = "New passwords do not match!";
+	} else {
+		var url;
+		url = '/cgi-bin/db_manager.py?';
+		url += 'update_user=' + encodeURIComponent('update');
+		url += '&username=' + encodeURIComponent(username);
+		url += '&password=' + encodeURIComponent(password);
+		url += '&new_username=' + encodeURIComponent(new_username);
+		url += '&new_password=' + encodeURIComponent(new_password);
+		url += '&new_email=' + encodeURIComponent(new_email);
 		callAjax(url, requestCallback);
 	}
 }
